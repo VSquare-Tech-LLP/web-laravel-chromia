8698f7cfe13c1577998b820b3a266f32a44faccd
Nikhil <nikhil@eyuva.com>
2022-05-10 16:37:56 +0530

    menu manager trans file

---
+++ resources/lang/en/strings.php
@@ -0,0 +1,69 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Strings Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in strings throughout the system.
+    | Regardless where it is placed, a string can be listed here so it is easily
+    | found in a intuitive way.
+    |
+    */
+
+    'backend' => [
+        'menu_manager' => [
+            'title' => 'Menu Manager',
+            'edit_menus' => 'Edit Menus',
+            'locations' => 'Locations',
+            'select_to_edit' => 'Select the menu you want to edit',
+            'create_new' => 'Create new menu',
+            'or' => 'Or',
+            'choose' => 'Choose',
+            'welcome' => 'Welcome',
+            'url' => 'URL',
+            'label' => 'Label',
+            'add_to_menu' => 'Add to Menu',
+            'screen_reader_text' => 'Press return or enter to expand',
+            'categories' => 'Categories',
+            'category' => 'Category',
+            'pages' => 'Pages',
+            'page' => 'Page',
+            'posts' => 'Posts',
+            'post' => 'Post',
+            'link' => 'Link',
+            'custom_link' => 'Custom Link',
+            'menu_structure' => 'Menu Structure',
+            'name' => 'Name',
+            'create_menu' => 'Create Menu',
+            'save_menu' => 'Save Menu',
+            'delete_menu' => 'Delete Menu',
+            'sub_menu' => 'Sub-menu',
+            'menu_creation' => 'Menu Creation',
+            'drag_instruction_1' => 'Place each item in the order you prefer. Click on the arrow to the right of the item to display more configuration options.',
+            'drag_instruction_2' => 'Please enter the name and select "Create menu" button',
+            'class' => 'Class CSS (optional)',
+            'move' => 'Move',
+            'move_up' => 'Move up',
+            'move_down' => 'Move down',
+            'move_right' => 'Move right',
+            'move_left' => 'Move left',
+            'top' => 'Top',
+            'delete' => 'Delete',
+            'cancel' => 'Cancel',
+            'update_item' => 'Update Item',
+            'menu_settings' => 'Menu Settings',
+            'auto_add_pages' => 'Auto Add Pages',
+            'auto_add_pages_desc' => 'Automatically add new top-level pages to this menu',
+            'display' => 'Display',
+            'top_menu' => 'Top Menu',
+            'footer_menu' => 'Footer Menu',
+            'currently' => 'Currently set to',
+            'theme_location' => 'Theme Location',
+            'save_changes' => 'Save Changes',
+            'assigned_menu' => 'Assigned Menu',
+            'edit' => 'Edit',
+            'select_all' => 'Select All',
+        ],
+    ]
+];
