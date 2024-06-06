f0e250daac07cc6d210e02c75f93291267cc70f6
Savan Kaneriya <kaneriyasavan@gmail.com>
2024-06-06 17:56:33 +0530

    Fix watermark try - 3

--- app/Http/Controllers/FaceSwapController.php
+++ app/Http/Controllers/FaceSwapController.php
@@ -62,7 +62,7 @@ class FaceSwapController extends Controller
             $result = $goApiService->swapResult($task_uuid);
 
             if ($result && $result->status == "success") {
-                if ($request->has('watermark') && ($request->watermark == true || $request->watermark == "true")) {
+                if ($request->has('watermark') && $request->watermark === "true") {
                     Log::info("Generated with watermark = ", [$request->watermark]);
                     $localImage = $this->storeLocaly($result->image, true);
                 } else {
