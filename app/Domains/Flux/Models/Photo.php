041a34d0e016a35c08160e4a5bb1c3499c8a9802
Savan Kaneriya <savan@vsquare.tech>
2024-08-16 23:21:24 +0530

    Added thumb_url

--- app/Domains/Flux/Models/Photo.php
+++ app/Domains/Flux/Models/Photo.php
@@ -9,5 +9,10 @@ use Illuminate\Database\Eloquent\Model;
 class Photo extends Model
 {
     protected $guarded = [];
+    protected $appends = ['thumb_url'];
+    public function getThumbUrlAttribute()
+    {
+        return str_replace('source_images', 'source_thumbs', $this->url);
+    }
 
 }
